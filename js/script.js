2715241fb7e1ff840d7b1e5e228cd1930b000153
AsiaZmudziak <asiazmudziak@gmail.com>
2020-10-22 04:25:45 +0100

    Add js code generate links to tags list

--- js/script.js
+++ js/script.js
@@ -88,8 +88,8 @@ function generateTitleLinks() {
 generateTitleLinks();
 
 function generateTags() {
-  /* [NEW] create a new variable allTags with an empty array */
-  let allTags = [];
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
 
     /* [DONE] find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
@@ -124,11 +124,14 @@ function generateTags() {
     /* [DONE]add generated code to html variable */
     html = html + linkHTML;
     console.log(html);
-  /* [NEW] check if this link is NOT already in allTags */
-  if (allTags.indexOf(linkHTML) == -1) {
-    /* [NEW] add generated code to allTags array */
-    allTags.push(linkHTML);
-  }
+
+ /* [NEW] check if this link is NOT already in allTags */
+  if (!allTags.hasOwnProperty(tag)) {
+/* [NEW] add tag to allTags object */
+allTags[tag] = 1;
+} else {
+  allTags[tag]++;
+}
     
   /* END LOOP: for each tag */
   
@@ -140,8 +143,27 @@ function generateTags() {
     /* [NEW] find list of tags in right column */
   const tagList = document.querySelector('.tags');
 
-  /* [NEW] add html from allTags to tagList */
-  tagList.innerHTML = allTags.join(' ');
+  /* [NEW] create variable for all links HTML code */
+  let allTagsHTML = '';
+
+  /* [NEW] START LOOP: for each tag in alltags: */
+  for (let tag in allTags) {
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML +=
+      '<li><a href="#tag-' +
+      tag +
+      '">' +
+      tag +
+      ' (' +
+      allTags[tag] +
+      ') ' +
+      '</a></li>';
+  }
+
+  /* [NEW] END LOOP: for each tag in alltags: */
+
+  /* [NEW] add html from alltagsHTML to tagList */
+  tagList.innerHTML = allTagsHTML;
     }
 }
 }
